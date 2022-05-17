cd13484f9038a26b8feb929a9c5f94877e93ed72
pablogarciaar <pablogarcia.ar@gmail.com>
2022-05-17 15:59:00 -0300

    se agregan imports

--- src/gestionrecetas/GestionRecetas.java
+++ src/gestionrecetas/GestionRecetas.java
@@ -1,8 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package gestionrecetas;
 
 /**
  *
